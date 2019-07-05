f0b47f3e57edd07f4a9437d8b9248b74c7e7c4c2
Kevin Susanto <kevin.stnt@gmail.com>
2019-07-05 15:10:47 +0700

    Initial Commit

---
+++ Core Data Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Core Data Test
+//
+//  Created by Kevin Susanto on 05/07/19.
+//  Copyright © 2019 Kevin Susanto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
